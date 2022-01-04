6a7c5de6b412b43db8413276a43c5b8e98e3dfab
neonfighter28 <58990354+neonfighter28@users.noreply.github.com>
2022-01-04 16:36:29 +0100

    formatting

--- get-data.py
+++ get-data.py
@@ -1,25 +1,23 @@
 """
-Usage: py get-data.py [country]
-If parameter isn't specified, switzerland will be used
+Usage: py get-data.py [country='switzerland'] [no_cache=False]
 
 Goals:
 Plot containing 4 lines:
 1. Traffic Data of country -> walking | DONE
 2. Traffic Data of country -> driving | DONE
 3. Traffic Data of country -> transit | DONE
-4. COVID Data of Country              | DONE
+4. Traffic data normalized            | DONE
+5. COVID Data of country              | DONE
 
-5. New dataset on whether there was a lockdown or not
+6. New dataset on whether there was a lockdown or not
 
     Dates of lockdown:
     - 16.03.20-11.05.20,
     - 18.10.20-17.02.21,
 
     -> different levels of lockdowns?
-    - mask mandate
-    -
 
-Traffic Data needs to be normalized, to account for weekends/days off
+Traffic Data needs to be normalized, to account for weekends/days off | DONE
 
 -> Predict COVID Data for the next 2 weeks
 -> Predict traffic Data for the next 2 weeks
@@ -160,12 +158,22 @@ def get_current_apple_url():
 
 timestart = time.perf_counter()
 
+logger.debug("%s", "log")
 try:
-    country = argv[1]
+    COUNTRY = argv[1]
 except IndexError:
-    country = "switzerland" # default
+    COUNTRY = "switzerland"  # default
+try:
+    if argv[2] == "False":
+        CACHE = False
+    else:
+        CACHE = True
+except IndexError:
+    CACHE = True
 
-confirmed_df, apple_mobility = get_data()
+logger.debug("%s", f"Arguments COUNTRY={COUNTRY}, CACHE={CACHE}")
+
+confirmed_df, apple_mobility = get_data(CACHE)
 
 cols = confirmed_df.keys()
 
@@ -180,29 +188,28 @@ for i in dates:
     # confirmed, deaths, recovered, and active
     world_cases.append(confirmed_sum)
 
-# window size
-window = 7
-
 # confirmed cases
 world_daily_increase = daily_increase(world_cases)
-world_confirmed_avg= moving_average(world_cases, window)
-world_daily_increase_avg = moving_average(world_daily_increase, window)
+world_confirmed_avg = moving_average(world_cases)
+world_daily_increase_avg = moving_average(world_daily_increase)
 
 days_since_1_22 = np.array([i for i in range(len(dates))]).reshape(-1, 1)
 world_cases = np.array(world_cases).reshape(-1, 1)
 
 days_in_future = 40
-future_forecast = np.array([i for i in range(len(dates)+days_in_future)]).reshape(-1, 1)
+future_forecast = np.array(
+    [i for i in range(len(dates)+days_in_future)]).reshape(-1, 1)
 adjusted_dates = future_forecast[:-10]
 
 start = '1/22/2020'
 start_date = datetime.datetime.strptime(start, '%m/%d/%Y')
 future_forecast_dates = []
 for i in range(len(future_forecast)):
-    future_forecast_dates.append((start_date + datetime.timedelta(days=i)).strftime('%m/%d/%Y'))
+    future_forecast_dates.append(
+        (start_date + datetime.timedelta(days=i)).strftime('%m/%d/%Y'))
 
 #plt.plot(future_forecast_dates, future_forecast)
-#plt.show()
+# plt.show()
 
 # slightly modify the data to fit the model better (regression models cannot pick the pattern)
 days_to_skip = 500
@@ -211,15 +218,15 @@ X_train_confirmed, X_test_confirmed, y_train_confirmed, y_test_confirmed = train
     world_cases[days_to_skip:],
     test_size=0.08,
     shuffle=False
-    )
+)
 
 # Get Covid data for country
 timestart = time.perf_counter()
 
 try:
     for index, value in enumerate(confirmed_df.loc):
-        if confirmed_df.loc[index]["Country/Region"].upper() == country.upper():
-            continue
+        if confirmed_df.loc[index]["Country/Region"].upper() == COUNTRY.upper():
+            break
 except KeyError:
     pass
 
@@ -235,8 +242,8 @@ except KeyError:
 
 new_data_def = def_data.copy()
 
-# Convert total cases each day to daily incidence-
-lst = [0]
+# Convert total cases each day to daily incidence
+lst = [0, 0, 0, 0, 0, 0, 0, 0]
 k_minus_1 = 0
 for index, (k, v) in enumerate(def_data.items()):
     if index < 5:
@@ -247,7 +254,7 @@ for index, (k, v) in enumerate(def_data.items()):
         k_minus_1 = v
 
 # Create Data Structures
-datasets_as_xy = prep_apple_mobility_data(apple_mobility, country)
+datasets_as_xy = prep_apple_mobility_data(apple_mobility, COUNTRY)
 
 adjusted_dates = adjusted_dates.reshape(1, -1)[0]
 plt.figure(figsize=(16, 10))
@@ -258,36 +265,42 @@ ax2 = ax.twinx()
 # Get average of all lists
 data_rows = []
 
-for z, value in enumerate(datasets_as_xy):
+
+for z, value in tqdm(enumerate(datasets_as_xy)):
     data_x = [i[0] for i in value]
     data_y = interp_nans([i[1] for i in value])
     data_rows.append(moving_average(data_y, 7))
 
     match z:
         case 0:
-            ax.plot(data_x, moving_average(data_y, 7), color="#FE9402", label="Driving", alpha=0.5)
+            ax.plot(data_x, moving_average(data_y, 7),
+                    color="#FE9402", label="Driving", alpha=0.5)
         case 1:
-            ax.plot(data_x, moving_average(data_y, 7), color="#FE2D55", label="Transit", alpha=0.5)
+            ax.plot(data_x, moving_average(data_y, 7),
+                    color="#FE2D55", label="Transit", alpha=0.5)
         case 2:
-            ax.plot(data_x, moving_average(data_y, 7), color="#AF51DE", label="Walking", alpha=0.5)
+            ax.plot(data_x, moving_average(data_y, 7),
+                    color="#AF51DE", label="Walking", alpha=0.5)
         case _:
             ax.plot(data_x, moving_average(data_y, 7), color="black")
 
 avg_traffic_data = moving_average([sum(e)/len(e) for e in zip(*data_rows)], 7)
 ax.plot(data_x, avg_traffic_data, color="green", label="Average mobility data")
-
+ax.set_ylim(ymax=200)
 ax2.plot(
     data_x[2:],
     moving_average(lst, 7),
     color="blue",
-    label=f"Incidence {country}, moving average"
-    )
-ax2.set_ylim(ymax=max(lst))
+    label=f"Incidence {COUNTRY}, moving average"
+)
+ax2.set_ylim(ymax=average(sorted(lst, reverse=True)[:2]))
 #ax2.plot(data_x[2:], world_daily_increase, color="salmon", label="Daily Incidence")
 #ax2.plot(data_x[2:], world_daily_increase_avg, color="red", label="Daily Incidence, normalized")
 plt.xlabel('Days Since 1/22/2020', size=15)
-ax.set_ylabel(' Increase of traffic routing requests in %, baseline at 100', size = 20)
-plt.xticks(size=10, rotation=180, ticks=[i*50 for i in range(len(data_x)%50)])
+ax.set_ylabel(
+    ' Increase of traffic routing requests in %, baseline at 100', size=20)
+plt.xticks(size=10, rotation=180, ticks=[
+    i*50 for i in range(len(data_x) % 50)])
 plt.yticks(size=10)
 plt.grid()
 #print(avg_traffic_data[2:], moving_average(lst,7))
@@ -296,18 +309,19 @@ plt.grid()
 n_traffic_data = normalize(moving_average(avg_traffic_data, 50))
 n_daily_incidence = normalize(moving_average(lst, 50))
 
-print(pearsonr(n_traffic_data[2:], n_daily_incidence))
+logging.info("%s", f"Pearson Constant: {pearsonr(n_traffic_data[2:], n_daily_incidence)}")
 
-if country.lower() =="switzerland":
+if COUNTRY.lower() == "switzerland":
     plt.axvspan(
-        63, # 16.03.20
+        63,  # 16.03.20
         119,
-        color='red', alpha=0.5)
+        color='red', alpha=0.5
+    )
 
     plt.axvspan(279, 402, color="red", alpha=0.5)
-    ax.legend()
+ax.legend()
 ax2.legend()
-#plt.legend([f"Traffic requests for {country}"], loc=9)
+#plt.legend([f"Traffic requests for {COUNTRY}"], loc=9)
 print(time.perf_counter() - timestart)
-#exit(1)
+# exit(1)
 plt.show()
