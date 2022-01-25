60164f44100b458222135d80b60cdcc14dc4cba4
neonfighter28 <58990354+neonfighter28@users.noreply.github.com>
2022-01-25 17:39:24 +0100

    update tests to account for os

--- testing.py
+++ testing.py
@@ -1,9 +1,16 @@
 #!/bin/python
 
 import os
+import sys
 from config import TEST_CASES as TESTS
 
-PYTHON_PATH = f"{os.getcwd()}/venv/bin/python"
-
-for i in TESTS:
-    os.system(f"echo {i} | {PYTHON_PATH} main.py")
+if os.name == "nt":
+    PYTHON_PATH = f"{os.getcwd()}\\venv\\scripts\\python.exe"
+elif os.name == "posix":
+    PYTHON_PATH = f"{os.getcwd()}/venv/bin/python"
+try:
+    if sys.argv[1] == "--full":
+        for i in TESTS:
+            os.system(f"echo {i} | {PYTHON_PATH} main.py")
+except IndexError:
+    os.system(f"echo {TESTS[0]} | {PYTHON_PATH} main.py")
